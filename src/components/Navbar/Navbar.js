574efeb88f31d8fe75b744b6be54b83f51efcdad
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-11 11:14:16 -0800

    signed up for Font Awesome and researching different fonts for the project right now

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { MenuItems } from "./MenuItems"
 
 class Navbar extends React.Component {
-    render (){
+    render() {
         return (
             <nav className="navbarItems">
-                <h1 className = "navbar-logo">Sara's Corner</h1>
-                <div className = "menu-Icon"></div>
+                <h1 className="navbar-logo">Sara's Corner</h1>
+                <div className="menu-Icon">
+
+                </div>
+                <ul>
+                    {MenuItems.map((item, index) => {
+                        return (
+                                <li key={index}>
+                                    <a className={item.cName} href={item.link}>
+                                        {item.title}
+                                    </a>
+                                </li>
+                                )
+                         })}
+                </ul>
             </nav>
         )
     }
 }
+
+export default Navbar
