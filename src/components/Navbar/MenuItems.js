859b57bc6470e837ecc861e50d53cf795af0088b
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-11 10:42:57 -0800

    made the menu array for my navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,38 @@
+const MenuItem = [
+    {
+        title:"Home",
+        link:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Recipes",
+        link:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Crafts",
+        link:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Travel",
+        link:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"About Me",
+        link:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Contact Me",
+        link:'#',
+        cName:'nav-links'
+    },
+    {
+        title:"Sign In",
+        link:'#',
+        cName:'nav-links-mobile'
+    },
+    
+]
