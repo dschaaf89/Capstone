21dd0b6347b827e36c3d85d8582743ed108b19f5
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-11 10:20:38 -0800

    starting to build out the custom Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function Header(){
     return (
-      <p>hi</p>  
+      
     );
 }
 export default Header
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render (){
+        return (
+            <nav className="navbarItems">
+                <h1 className = "navbar-logo">Sara's Corner</h1>
+                <div className = "menu-Icon"></div>
+            </nav>
+        )
+    }
+}
