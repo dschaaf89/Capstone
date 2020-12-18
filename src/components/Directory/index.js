2f6fef6ff528999fb1ced00f59d4466c55ee63a3
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-18 14:13:57 -0800

    need to do the styling but almost done with the splash page

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import Food from './../../assets/Pasta.jpg';
+import Craft from './../../assets/Wreaths.jpg';
+import AboutMe from './../../assets/AboutMe.jpg';
+import Travel from './../../assets/Travel.jpeg';
 
+import CraftStore from "./../../assets/Craft-Store.jpg";
 
 const Directory = props => {
   return(
-    <div>
+    <div className="Directory">
+      <div className="wrap">
+        <div 
+        className="item"
+        style={{
+          backgroundImage:`url(${Food})`
+        }}>
+          <a>InTheKitchen</a>
+        </div>
+
+         <div 
+        style={{
+          backgroundImage:`url(${Craft})`
+        }}>
+          <a>CraftingCorner</a>
+        </div>
+
+        <div 
+        style={{
+          backgroundImage:`url(${Travel})`
+        }}>
+          <a>MyGypsyHeart</a>
+        </div>
 
 
+
+         <div 
+        style={{
+          backgroundImage:`url(${AboutMe})`
+        }}>
+          <a>AboutMe</a>
+        </div>
+
+
+
+         <div 
+        style={{
+          backgroundImage:`url(${CraftStore})`
+        }}>
+          <a>CraftStore</a>
+        </div>
+      </div>
     </div>
   )
 }
