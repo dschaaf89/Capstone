8d24eb6b8e477cb423ef16f1a079fd3fb550b6bf
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-18 13:50:41 -0800

    making new folders to make this new splash page for project

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
-import Navbar from './Navbar/Navbar';
-import InTheKitchenController from './InTheKitchen/InTheKitchenController';
-import CraftingCornerController from './CraftingCorner/CraftingCornerController'
-import AboutMe from './AboutMe/AboutMe'
-import React from 'react'
-import './App.css';
+// import Navbar from './Navbar/Navbar';
+// import InTheKitchenController from './InTheKitchen/InTheKitchenController';
+// import CraftingCornerController from './CraftingCorner/CraftingCornerController';
+// import AboutMe from './AboutMe/AboutMe';
+import React from 'react';
+import Homepage from '../pages/Homepage';
+//import './App.css';
+import './default.scss';
+import Header from "./Header";
 
 function App() {
 return(
    
   <React.Fragment>
-     <Navbar />
-     {/* <InTheKitchenController/> */}
-     <CraftingCornerController />
-     {/* <AboutMe /> */}
+    <Header/>
+    <Homepage/>
      
   </React.Fragment>
 );
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+import Navbar from '../Navbar/Navbar'
+
+const Header = props => {
+  return (
+    <header className = "header">
+      <Navbar/>
+    </header>
+  );
+};
+
+export default Header;
+ 
