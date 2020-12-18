60bdff53a84cb25588415613ec23bf69230cfee9
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-12-18 14:54:18 -0800

    made a splash page

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -3,55 +3,66 @@ import Food from './../../assets/Pasta.jpg';
 import Craft from './../../assets/Wreaths.jpg';
 import AboutMe from './../../assets/AboutMe.jpg';
 import Travel from './../../assets/Travel.jpeg';
-
 import CraftStore from "./../../assets/Craft-Store.jpg";
+import './styles.scss';
 
 const Directory = props => {
-  return(
-    <div className="Directory">
+  return (
+    <div className="directory">
       <div className="wrap">
-        <div 
-        className="item"
-        style={{
-          backgroundImage:`url(${Food})`
-        }}>
-          <a>InTheKitchen</a>
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${Food})`
+          }}
+        >
+          <a>
+            InTheKitchen
+          </a>
         </div>
-
-         <div 
-        style={{
-          backgroundImage:`url(${Craft})`
-        }}>
-          <a>CraftingCorner</a>
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${Craft})`
+          }}
+        >
+          <a>
+            CraftingCorner
+          </a>
         </div>
-
-        <div 
-        style={{
-          backgroundImage:`url(${Travel})`
-        }}>
-          <a>MyGypsyHeart</a>
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${Travel})`
+          }}
+        >
+          <a>
+            MyGypsyHeart
+          </a>
         </div>
-
-
-
-         <div 
-        style={{
-          backgroundImage:`url(${AboutMe})`
-        }}>
-          <a>AboutMe</a>
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${AboutMe})`
+          }}
+        >
+          <a>
+            AboutMe
+          </a>
         </div>
-
-
-
-         <div 
-        style={{
-          backgroundImage:`url(${CraftStore})`
-        }}>
-          <a>CraftStore</a>
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${CraftStore})`
+          }}
+        >
+          <a>
+            Craft-Store
+          </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Directory;
